4e3d088aa8fb92a240ee35aa4020bdb328fe59f3
dslul <danielelaudani@protonmail.com>
2020-09-07 18:56:07 +0200

    remove unneeded cleanupInternalStateForFinishInput

--- app/src/main/java/org/dslul/openboard/inputmethod/latin/LatinIME.java
+++ app/src/main/java/org/dslul/openboard/inputmethod/latin/LatinIME.java
@@ -1377,7 +1377,6 @@ public class LatinIME extends InputMethodService implements KeyboardActionListen
         if (mInputLogic.mConnection.hasSelection()) {
             mInputLogic.sendDownUpKeyEvent(KeyEvent.KEYCODE_DEL);
             mInputLogic.sendDownUpKeyEvent(KeyEvent.KEYCODE_SHIFT_LEFT);
-            cleanupInternalStateForFinishInput();
         }
     }
 
