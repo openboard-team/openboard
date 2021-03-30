f51aae77049b1337c574fc3f470343432c17868b
Leonardo Quevedo <leonardoq@trin.ca>
2021-03-30 19:21:49 -0300

    🎉feat(jni): Adds Android native libraries.

--- app/src/main/java/org/dslul/openboard/inputmethod/latin/utils/JniUtils.java
+++ app/src/main/java/org/dslul/openboard/inputmethod/latin/utils/JniUtils.java
@@ -23,7 +23,7 @@ import org.dslul.openboard.inputmethod.latin.define.JniLibName;
 public final class JniUtils {
     private static final String TAG = JniUtils.class.getSimpleName();
 
-    public static boolean sHaveGestureLib = false;
+    public static boolean sHaveGestureLib = true;
     static {
         try {
             System.loadLibrary(JniLibName.JNI_LIB_NAME);
