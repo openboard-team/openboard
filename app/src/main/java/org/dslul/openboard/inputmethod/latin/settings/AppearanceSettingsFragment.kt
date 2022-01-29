551beb69a384c3f9ac2c35f868511c94df56b9ae
pdroidandroid@gmail.com <MajeurAndroid@users.noreply.github.com>
2022-01-29 15:58:04 +0100

    Implemented new theme settings

--- app/src/main/java/org/dslul/openboard/inputmethod/latin/settings/AppearanceSettingsFragment.kt
+++ app/src/main/java/org/dslul/openboard/inputmethod/latin/settings/AppearanceSettingsFragment.kt
@@ -13,85 +13,180 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package org.dslul.openboard.inputmethod.latin.settings
 
-package org.dslul.openboard.inputmethod.latin.settings;
-
-import android.content.SharedPreferences;
-import android.os.Bundle;
-
-import org.dslul.openboard.inputmethod.latin.R;
-import org.dslul.openboard.inputmethod.latin.common.Constants;
-import org.dslul.openboard.inputmethod.latin.define.ProductionFlags;
-
-import java.util.Locale;
+import android.os.Build
+import android.os.Bundle
+import android.preference.ListPreference
+import android.preference.Preference
+import android.preference.TwoStatePreference
+import org.dslul.openboard.inputmethod.keyboard.KeyboardTheme
+import org.dslul.openboard.inputmethod.latin.R
+import org.dslul.openboard.inputmethod.latin.common.Constants
+import org.dslul.openboard.inputmethod.latin.define.ProductionFlags
+import java.util.*
 
 /**
  * "Appearance" settings sub screen.
  */
-public final class AppearanceSettingsFragment extends SubScreenFragment {
-    @Override
-    public void onCreate(final Bundle icicle) {
-        super.onCreate(icicle);
-        addPreferencesFromResource(R.xml.prefs_screen_appearance);
+class AppearanceSettingsFragment : SubScreenFragment(), Preference.OnPreferenceChangeListener {
+
+    private var selectedThemeId = 0
+
+    private lateinit var themeFamilyPref: ListPreference
+    private lateinit var themeVariantPref: ListPreference
+    private lateinit var keyBordersPref: TwoStatePreference
+    private lateinit var amoledModePref: TwoStatePreference
+    private var dayNightPref: TwoStatePreference? = null
+
+
+    override fun onCreate(icicle: Bundle?) {
+        super.onCreate(icicle)
+        addPreferencesFromResource(R.xml.prefs_screen_appearance)
+        val keyboardTheme = KeyboardTheme.getKeyboardTheme(activity)
+        selectedThemeId = keyboardTheme.mThemeId
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+            removePreference(Settings.PREF_THEME_DAY_NIGHT)
+        }
+        setupTheme()
+
         if (!ProductionFlags.IS_SPLIT_KEYBOARD_SUPPORTED ||
-                Constants.isPhone(Settings.readScreenMetrics(getResources()))) {
-            removePreference(Settings.PREF_ENABLE_SPLIT_KEYBOARD);
+                Constants.isPhone(Settings.readScreenMetrics(resources))) {
+            removePreference(Settings.PREF_ENABLE_SPLIT_KEYBOARD)
         }
         setupKeyboardHeight(
-                Settings.PREF_KEYBOARD_HEIGHT_SCALE, SettingsValues.DEFAULT_SIZE_SCALE);
+                Settings.PREF_KEYBOARD_HEIGHT_SCALE, SettingsValues.DEFAULT_SIZE_SCALE)
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();
+    override fun onResume() {
+        super.onResume()
+        updateThemePreferencesState()
         CustomInputStyleSettingsFragment.updateCustomInputStylesSummary(
-                findPreference(Settings.PREF_CUSTOM_INPUT_STYLES));
-        ThemeSettingsFragment.updateKeyboardThemeSummary(findPreference(Settings.SCREEN_THEME));
+                findPreference(Settings.PREF_CUSTOM_INPUT_STYLES))
     }
 
-    private void setupKeyboardHeight(final String prefKey, final float defaultValue) {
-        final SharedPreferences prefs = getSharedPreferences();
-        final SeekBarDialogPreference pref = (SeekBarDialogPreference)findPreference(prefKey);
-        if (pref == null) {
-            return;
+    override fun onPreferenceChange(preference: Preference, value: Any?): Boolean {
+        (preference as? ListPreference)?.apply {
+            summary = entries[entryValues.indexOfFirst { it == value }]
         }
-        pref.setInterface(new SeekBarDialogPreference.ValueProxy() {
-            private static final float PERCENTAGE_FLOAT = 100.0f;
-            private float getValueFromPercentage(final int percentage) {
-                return percentage / PERCENTAGE_FLOAT;
-            }
+        saveSelectedThemeId()
+        return true
+    }
 
-            private int getPercentageFromValue(final float floatValue) {
-                return (int)(floatValue * PERCENTAGE_FLOAT);
-            }
+    private fun saveSelectedThemeId(
+            family: String = themeFamilyPref.value,
+            variant: String = themeVariantPref.value,
+            keyBorders: Boolean = keyBordersPref.isChecked,
+            dayNight: Boolean = dayNightPref?.isChecked ?: false,
+            amoledMode: Boolean = amoledModePref.isChecked
+    ) {
+        selectedThemeId = KeyboardTheme.getThemeForParameters(family, variant, keyBorders, dayNight, amoledMode)
+        KeyboardTheme.saveKeyboardThemeId(selectedThemeId, sharedPreferences)
+    }
 
-            @Override
-            public void writeValue(final int value, final String key) {
-                prefs.edit().putFloat(key, getValueFromPercentage(value)).apply();
+    private fun updateThemePreferencesState(skipThemeFamily: Boolean = false, skipThemeVariant: Boolean = false) {
+        val themeFamily = KeyboardTheme.getThemeFamily(selectedThemeId)
+        val isLegacyFamily = KeyboardTheme.THEME_FAMILY_HOLO == themeFamily
+        if (!skipThemeFamily) {
+            themeFamilyPref.apply {
+                value = themeFamily
+                summary = themeFamily
             }
-
-            @Override
-            public void writeDefaultValue(final String key) {
-                prefs.edit().remove(key).apply();
+        }
+        val variants = KeyboardTheme.THEME_VARIANTS[themeFamily]!!
+        val variant = KeyboardTheme.getThemeVariant(selectedThemeId)
+        if (!skipThemeVariant) {
+            themeVariantPref.apply {
+                entries = variants
+                entryValues = variants
+                value = variant ?: variants[0]
+                summary = variant ?: "Auto"
+                isEnabled = isLegacyFamily || !KeyboardTheme.getIsDayNight(selectedThemeId)
             }
+        }
+        keyBordersPref.apply {
+            isEnabled = !isLegacyFamily && !KeyboardTheme.getIsAmoledMode(selectedThemeId)
+            isChecked = isLegacyFamily || KeyboardTheme.getHasKeyBorders(selectedThemeId)
+        }
+        amoledModePref.apply {
+            isEnabled = !isLegacyFamily && variant != KeyboardTheme.THEME_VARIANT_LIGHT
+                    && !KeyboardTheme.getHasKeyBorders(selectedThemeId)
+            isChecked = !isLegacyFamily && KeyboardTheme.getIsAmoledMode(selectedThemeId)
+        }
+        dayNightPref?.apply {
+            isEnabled = !isLegacyFamily
+            isChecked = !isLegacyFamily && KeyboardTheme.getIsDayNight(selectedThemeId)
+        }
+    }
 
-            @Override
-            public int readValue(final String key) {
-                return getPercentageFromValue(Settings.readKeyboardHeight(prefs, defaultValue));
+    private fun setupTheme() {
+        themeFamilyPref = preferenceScreen.findPreference(Settings.PREF_THEME_FAMILY) as ListPreference
+        themeFamilyPref.apply {
+            entries = KeyboardTheme.THEME_FAMILIES
+            entryValues = KeyboardTheme.THEME_FAMILIES
+            onPreferenceChangeListener = Preference.OnPreferenceChangeListener { _, value ->
+                summary = entries[entryValues.indexOfFirst { it == value }]
+                saveSelectedThemeId(family = value as String)
+                updateThemePreferencesState(skipThemeFamily = true)
+                true
             }
-
-            @Override
-            public int readDefaultValue(final String key) {
-                return getPercentageFromValue(defaultValue);
+        }
+        themeVariantPref = preferenceScreen.findPreference(Settings.PREF_THEME_VARIANT) as ListPreference
+        themeVariantPref.apply {
+            onPreferenceChangeListener = Preference.OnPreferenceChangeListener { _, value ->
+                summary = entries[entryValues.indexOfFirst { it == value }]
+                saveSelectedThemeId(variant = value as String)
+                updateThemePreferencesState(skipThemeFamily = true, skipThemeVariant = true)
+                true
             }
+        }
+        keyBordersPref = preferenceScreen.findPreference(Settings.PREF_THEME_KEY_BORDERS) as TwoStatePreference
+        keyBordersPref.onPreferenceChangeListener = Preference.OnPreferenceChangeListener { _, value ->
+            saveSelectedThemeId(keyBorders = value as Boolean)
+            updateThemePreferencesState(skipThemeFamily = true)
+            true
+        }
+        amoledModePref = preferenceScreen.findPreference(Settings.PREF_THEME_AMOLED_MODE) as TwoStatePreference
+        amoledModePref.onPreferenceChangeListener = Preference.OnPreferenceChangeListener { _, value ->
+            saveSelectedThemeId(amoledMode = value as Boolean)
+            updateThemePreferencesState(skipThemeFamily = true)
+            true
+        }
+        dayNightPref = preferenceScreen.findPreference(Settings.PREF_THEME_DAY_NIGHT) as? TwoStatePreference
+        dayNightPref?.onPreferenceChangeListener = Preference.OnPreferenceChangeListener { _, value ->
+            saveSelectedThemeId(dayNight = value as Boolean)
+            updateThemePreferencesState(skipThemeFamily = true)
+            true
+        }
+    }
 
-            @Override
-            public String getValueText(final int value) {
-                return String.format(Locale.ROOT, "%d%%", value);
-            }
+    private fun setupKeyboardHeight(prefKey: String, defaultValue: Float) {
+        val prefs = sharedPreferences
+        val pref = findPreference(prefKey) as? SeekBarDialogPreference
+        pref?.setInterface(object : SeekBarDialogPreference.ValueProxy {
 
-            @Override
-            public void feedbackValue(final int value) {}
-        });
+            private fun getValueFromPercentage(percentage: Int) =  percentage / PERCENTAGE_FLOAT
+
+            private fun getPercentageFromValue(floatValue: Float) = (floatValue * PERCENTAGE_FLOAT).toInt()
+
+            override fun writeValue(value: Int, key: String) = prefs.edit()
+                    .putFloat(key, getValueFromPercentage(value)).apply()
+
+            override fun writeDefaultValue(key: String) = prefs.edit().remove(key).apply()
+
+            override fun readValue(key: String) = getPercentageFromValue(
+                    Settings.readKeyboardHeight(prefs, defaultValue))
+
+            override fun readDefaultValue(key: String) = getPercentageFromValue(defaultValue)
+
+            override fun getValueText(value: Int) = String.format(Locale.ROOT, "%d%%", value)
+
+            override fun feedbackValue(value: Int) = Unit
+        })
+    }
+
+    companion object {
+        private const val PERCENTAGE_FLOAT = 100.0f
     }
 }
