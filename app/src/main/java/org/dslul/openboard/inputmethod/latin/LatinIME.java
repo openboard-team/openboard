55ef8efa822b81771caca651cb0ae1a4f4c75381
odmfl <odmfl@protonmail.com>
2023-01-02 15:43:37 +0100

    Add emoji search

--- app/src/main/java/org/dslul/openboard/inputmethod/latin/LatinIME.java
+++ app/src/main/java/org/dslul/openboard/inputmethod/latin/LatinIME.java
@@ -98,6 +98,7 @@ import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
 import javax.annotation.Nonnull;
+import org.dslul.openboard.inputmethod.latin.emojisearch.EmojiSearch;
 
 import static org.dslul.openboard.inputmethod.latin.common.Constants.ImeOption.FORCE_ASCII;
 import static org.dslul.openboard.inputmethod.latin.common.Constants.ImeOption.NO_MICROPHONE;
@@ -622,6 +623,7 @@ public class LatinIME extends InputMethodService implements KeyboardActionListen
         AudioAndHapticFeedbackManager.init(this);
         AccessibilityUtils.init(this);
         mStatsUtilsManager.onCreate(this /* context */, mDictionaryFacilitator);
+		EmojiSearch.init(this);
         super.onCreate();
 
         mClipboardHistoryManager.onCreate();
