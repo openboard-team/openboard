739a8dde37d56f961ffdbafa707d674998d8e340
odmfl <77236251+odmfl@users.noreply.github.com>
2023-01-04 18:54:27 +0100

    Fix suggestions again
    in the previous commit, some apps still didn't show the hints, now it's totally fixed

--- app/src/main/java/org/dslul/openboard/inputmethod/latin/InputAttributes.java
+++ app/src/main/java/org/dslul/openboard/inputmethod/latin/InputAttributes.java
@@ -105,13 +105,7 @@ public final class InputAttributes {
 
         // TODO: Have a helper method in InputTypeUtils
         // Make sure that passwords are not displayed in {@link SuggestionStripView}.
-        final boolean shouldSuppressSuggestions = mIsPasswordField
-                || InputTypeUtils.isEmailVariation(variation)
-                || InputType.TYPE_TEXT_VARIATION_URI == variation
-                || InputType.TYPE_TEXT_VARIATION_FILTER == variation
-                //|| flagNoSuggestions
-                || flagAutoComplete;
-        mShouldShowSuggestions = !shouldSuppressSuggestions;
+        mShouldShowSuggestions = !mIsPasswordField;
 
         mShouldInsertSpacesAutomatically = InputTypeUtils.isAutoSpaceFriendlyType(inputType);
 
